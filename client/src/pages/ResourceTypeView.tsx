6c53c62bcc76f8fe7a606362ee6b5053e64139a8
Roman Pogorov <roman.pogorov@gmail.com>
2025-04-07 17:59:36 +0300

    fix: prevent infinite loop in ResourceTypeView useEffect

--- client/src/pages/ResourceTypeView.tsx
+++ client/src/pages/ResourceTypeView.tsx
@@ -6,25 +6,24 @@ import { useMenu } from "../hooks/useMenu";
 const ResourceTypeView = () => {
   const params = useParams<{ id?: string }>();
   const { trackRecentItem, allMenuItems } = useMenu();
-  
+
   // If navigated to a specific resource, track it in recents
   useEffect(() => {
     if (params.id) {
-      const item = allMenuItems.find(item => item.id === params.id);
+      const item = allMenuItems.find((item) => item.id === params.id);
       if (item) {
-        trackRecentItem({
+        const recentItem = {
           id: item.id,
           name: item.name,
           category: item.category,
           icon: item.icon,
-        });
+        };
+        trackRecentItem(recentItem);
       }
     }
-  }, [params.id, trackRecentItem, allMenuItems]);
-  
-  return (
-    <ResourcesTable />
-  );
+  }, [params.id]);
+
+  return <ResourcesTable />;
 };
 
 export default ResourceTypeView;
