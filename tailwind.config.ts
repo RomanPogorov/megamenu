e63297b9168dd1b465abe5759d2afe4846f5a7a6
Roman Pogorov <roman.pogorov@gmail.com>
2025-04-08 16:56:21 +0300

    Update styles and components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: ["class"],
@@ -83,6 +84,9 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
